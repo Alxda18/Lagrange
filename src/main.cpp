14b02f8b229042ca58eee51fa94455dccf668c50
Alxda18 <143459856+Alxda18@users.noreply.github.com>
2023-12-03 23:29:52 -0600

    Menu3Clases

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,6 @@ public:
     ~main() {}
 };
 
-
 #include <iostream>
 #include <cstdlib>
 #include <iomanip>
@@ -15,28 +14,24 @@ public:
 
 using namespace std;
 
-const int MAX_GRADO = 10; 
-
+const int MAX_GRADO = 10;
 
 int main()
 {
-    int opcion, grado,n;
+    int opcion, grado, n;
     bool repetir = true;
     double datos[MAX_GRADO + 1][2];
     double resultadoInterpolacion;
     bool datosIngresados = false;
-     
-     
+
     cout << "Ingrese el grado del polinomio ( Menor igual a " << MAX_GRADO << "): ";
     cin >> grado;
 
-    if (grado > MAX_GRADO) {
+    if (grado > MAX_GRADO)
+    {
         cout << "El grado del polinomio no puede ser mayor de " << MAX_GRADO << ".\n";
         return 1;
     }
-    
-    
-    
 
     return 0;
 }
